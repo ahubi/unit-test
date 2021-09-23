cca50b7335b431ce9e155bb31e7a40e958c392ef
ahubi <ahxyz@web.de>
2021-09-23 15:11:34 +0200

    Fix build errors

--- tests/main.cpp
+++ tests/main.cpp
@@ -1,11 +1,13 @@
 #include <gtest/gtest.h>
 
-int main() {
+int main(int argc, char** argv) {
   // Demonstrate some basic assertions.
-  TEST(HelloTest, BasicAssertions) {
+  ::testing::InitGoogleTest(&argc, argv);
+  return RUN_ALL_TESTS();
+}
+TEST(HelloTest, BasicAssertions) {
     // Expect two strings not to be equal.
     EXPECT_STRNE("hello", "world");
     // Expect equality.
     EXPECT_EQ(7 * 6, 42);
-  }
 }
